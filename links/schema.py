9e78d77697d8895285f96eeaaa639898724b2a07
mandelak <kazi.keith@gmail.com>
2021-02-21 19:45:22 +0300

    Set up mutations for Links app
    - Set up mutation for creating a single link

--- links/schema.py
+++ links/schema.py
@@ -14,3 +14,27 @@ class Query(graphene.ObjectType):
 
     def resolve_links(self, info, **kwargs):
         return Link.objects.all()
+
+
+class CreateLink(graphene.Mutation):
+    id = graphene.Int()
+    url = graphene.String()
+    description = graphene.String()
+
+    # arguments for mutation
+    class Arguments:
+        url = graphene.String()
+        description = graphene.String()
+
+    # mutation method. Creates link and saves it to database
+    def mutate(self, info, url, description):
+        link = Link(url=url, description=description)
+        link.save()
+
+        # return CreateLink class with data just created
+        return CreateLink(id=link.id, url=link.url, description=link.description)
+
+
+# create mutation class with field to be resolved
+class Mutation(graphene.ObjectType):
+    create_link = CreateLink.Field()
